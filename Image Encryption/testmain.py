1d18cdd9cdd931f0e37fd48b0d671bdcbcfed490
Kali <csaidberk@gmail.com>
2021-07-19 04:50:39 -0400

    Img Sifreleme Basit Calisan Hal

---
+++ Image Encryption/testmain.py
@@ -0,0 +1,16 @@
+
+def openIMG(path):
+    pass
+
+def getData():
+    path = input("Lütfen İmaj Dosyası Seçiniz.")
+    imgData = openIMG()
+    return imgData
+
+def encryptIMG():
+    pass
+
+def main():
+    pass
+
+main()
